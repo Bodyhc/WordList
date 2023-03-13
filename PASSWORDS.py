4a467894b3bb31a6dbf040b3b757067fd67610f6
Abdullah M Soliman <94320243+Bodyhc@users.noreply.github.com>
2023-03-13 02:33:35 +0200

    Add files via upload

---
+++ PASSWORDS.py
@@ -0,0 +1,29 @@
+#CODE BY ABDULLAH M SOLIMAN 
+from itertools import product
+import string
+
+print("""
+████████╗██╗ ██████╗  ██████╗  ██████╗ 
+╚══██╔══╝██║██╔════╝ ██╔═══██╗██╔═══██╗
+   ██║   ██║██║  ███╗██║   ██║██║   ██║
+   ██║   ██║██║   ██║██║   ██║██║   ██║
+   ██║   ██║╚██████╔╝╚██████╔╝╚██████╔╝
+   ╚═╝   ╚═╝ ╚═════╝  ╚═════╝  ╚═════╝
+   MADE BY :- @TI9OO
+  """)
+
+min_len = int(input("Enter Your Min Len --> :"))
+max_len = int(input("Enter Your Max Len --> : "))
+counter = 0
+print("[+] Loding . . . ")
+charter = string.ascii_uppercase+string.ascii_lowercase+string.ascii_uppercase+string.digits+string.punctuation
+file_open = open("WordList.txt","w")
+
+for i in range(min_len,max_len+1):
+    for s in product(charter,repeat=i):
+        word = "".join(s)
+        file_open.write(word)
+        file_open.write("\n")
+        counter+=1
+
+print("WordList Genrate As {}".format(counter))
